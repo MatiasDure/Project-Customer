6efaad392d48d7c7743966300cfe683410604194
Matias Dure <497232@student.saxion.nl>
2022-09-08 10:29:06 +0200

    Added UI, store, and paused button (Not all functional currently)

--- Assets/Scripts/Resources.cs
+++ Assets/Scripts/Resources.cs
@@ -1,25 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Data.SqlTypes;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace Assets.Scripts
 {
     public class Resources: MonoBehaviour
     {
-        private int _money;
-        private int _food;
-        private int _medicine;
+        [SerializeField] private TextMeshProUGUI foodText;
+        [SerializeField] private TextMeshProUGUI medicText;
+        [SerializeField] private TextMeshProUGUI moneyText;
+
+        private int _moneyAmount;
+        private int _foodAmount;
+        private int _medicAmount;
 
         private static Resources _resource;
         public static Resources Resource { get => _resource;  }
-        public int Money { get => _money; }
-        public int Food { get => _food; }
-        public int Medicine { get => _medicine; }  
-        
-        public void AddMoney(int amount) => _money += amount;
-        public void AddFood(int amount) => _food += amount;
-        public void AddMedicine(int amount) => _medicine += amount;
-        
+        public int Money { get => _moneyAmount; }
+        public int Food { get => _foodAmount; }
+        public int Medicine { get => _medicAmount; }
+
+        private void Start()
+        {
+            _moneyAmount = 0;
+            _foodAmount = 0;
+            _medicAmount = 0;
+        }
+
+        private void Update() => UpdateUiText();  
+
+        private void UpdateUiText()
+        {
+            foodText.SetText(""+ _foodAmount);
+            medicText.SetText("" + _medicAmount);
+            moneyText.SetText("" + _moneyAmount);
+            Debug.Log(_foodAmount);
+        }
+        public void AddMoney(int amount) => _moneyAmount += amount;
+        public void AddFood(int amount) => _foodAmount += amount;
+        public void AddMedicine(int amount) => _medicAmount += amount;
+
     }
 }
