c91ddc708899c65909ff75cc9fb4ed9ad44f99ce
Matias Dure <497232@student.saxion.nl>
2022-09-22 14:33:57 +0200

    build fix

--- Assets/Scripts/Resources.cs
+++ Assets/Scripts/Resources.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Data.SqlTypes;
 using TMPro;
-using Unity.VisualScripting;
+//using Unity.VisualScripting;
 using UnityEngine;
 
 namespace Assets.Scripts
--- Assets/Scripts/Waypoints/WaypointsFollower.cs
+++ Assets/Scripts/Waypoints/WaypointsFollower.cs
@@ -1,6 +1,7 @@
 using Assets.Scripts;
 using System.Collections;
 using System.Collections.Generic;
+//using Unity.VisualScripting.ReorderableList;
 using UnityEngine;
 
 public class WaypointsFollower : MonoBehaviour
