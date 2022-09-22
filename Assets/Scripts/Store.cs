78d2d9a944960b0229c3f8f0336090e759bbaa79
tia <thelisafox@gmail.com>
2022-09-22 09:59:47 +0200

    Animations for Vignette. Posters, added lower walls

--- Assets/Scripts/Store.cs
+++ Assets/Scripts/Store.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.Rendering.Universal;
-using Unity.PlasticSCM.Editor.WebApi;
+//using Unity.PlasticSCM.Editor.WebApi;
 
 namespace Assets.Scripts
 {
